28bbf71b9d8a565bdbb289cf619a81ed864faf7a
Anton Barkovsky <swarmer.pm@gmail.com>
2012-03-24 12:29:43 +0300

    Epic refactoring

--- blast.py
+++ blast.py
@@ -7,23 +7,78 @@ import sys
 from os import path
 
 
-class Store:
-    DB_PATH = path.expanduser(path.join('~', '.blast.db'))
+class Blast:
+    DEFAULT_DB_PATH = path.expanduser(path.join('~', '.blast.db'))
 
     def __init__(self, path=None):
         if path is None:
-            self.db_path = self.DB_PATH
+            self.db_path = self.DEFAULT_DB_PATH
         else:
             self.db_path = path
         self.shelve = shelve.open(self.db_path)
         self.entries = self.shelve.get('entries', {})
         self.opened = True
 
-    def close(self):
-        self.shelve['entries'] = self.entries
-        self.shelve.close()
-        self.opened = False
+    @staticmethod
+    def is_valid_key(key):
+        reg = r'[\w]+(\.[\w]+)?$'
+        return re.match(reg, key)
 
+    def clear(self):
+        self.entries.clear()
+
+    def get_list(self, key=None):
+        if key is not None:
+            keys = [name for name in self if name.startswith(key + '.')]
+        else:
+            keys = list(self)
+        return sorted(keys)
+
+    @classmethod
+    def validate_key(cls, key):
+        if not cls.is_valid_key(key):
+            print('Error: Invalid key: `{0}`\n'.format(key) +
+                    'Expected format: `<word1>[.<word2>]` ' +
+                    'where words consist of letters, numbers and underscores')
+            sys.exit(1)
+
+    ### Commands ###
+    def cmd_get(self, args):
+        key = args.key
+        self.validate_key(key)
+        try:
+            print(self[key])
+        except KeyError:
+            print('Error: Key not found!')
+
+    def cmd_set(self, args):
+        key = args.key
+        self.validate_key(key)
+        value = args.value
+        if value is None:
+            value = sys.stdin.read()
+        self[key] = value
+
+    def cmd_delete(self, args):
+        key = args.key
+        self.validate_key(key)
+        try:
+            del self[key]
+        except KeyError:
+            print('Error: Key not found!')
+
+    def cmd_list(self, args):
+        key = args.key
+        if key is not None:
+            self.validate_key(key)
+        entries = self.get_list(key)
+        if not entries:
+            print('There are no entries!')
+            return
+        print('\n'.join(entries))
+    ###^ Commands ^###
+
+    ### Container interface ###
     def __getitem__(self, key):
         return self.entries[key]
 
@@ -41,6 +96,13 @@ class Store:
 
     def __contains__(self, item):
         return item in self.entries
+    ###^ Container interface ^###
+
+    ### Resource management ###
+    def close(self):
+        self.shelve['entries'] = self.entries
+        self.shelve.close()
+        self.opened = False
 
     def __enter__(self):
         return self
@@ -53,82 +115,36 @@ class Store:
     def __del__(self):
         if self.opened:
             self.close()
+    ###^ Resource management ^###
 
 
-def validate_key(key):
-    reg = r'[\w]+(\.[\w]+)?$'
-    if not re.match(reg, key):
-        print('Invalid key: `{0}`\n'.format(key) +
-                'Expected format: `<word1>[.<word2>]` ' +
-                'where words consist of letters, numbers and underscores')
-        sys.exit(1)
 
-### Commands ###
-def cmd_get(args):
-    with Store() as store:
-        key = args.key
-        validate_key(key)
-        try:
-            print(store[key])
-        except KeyError:
-            print('Key not found!')
 
-def cmd_set(args):
-    with Store() as store:
-        key = args.key
-        validate_key(key)
-        value = args.value
-        if value is None:
-            value = sys.stdin.read()
-        store[key] = value
+def main(args=None):
+    with Blast() as blast:
+        parser = argparse.ArgumentParser(prog='blast', 
+                                        description='Your command line key-value store')
+        subparsers = parser.add_subparsers(help='Sub-commands')
 
-def cmd_delete(args):
-    with Store() as store:
-        key = args.key
-        validate_key(key)
-        try:
-            del store[key]
-        except KeyError:
-            print('Key not found!')
+        get_parser = subparsers.add_parser('get', help='get help', description='Get value')
+        get_parser.add_argument('key')
+        get_parser.set_defaults(func=blast.cmd_get)
 
-def cmd_list(args):
-    with Store() as store:
-        if len(store) == 0:
-            print('There are no entries!')
-            return
-        key = args.key
-        if key is not None:
-            validate_key(key)
-            matching_entries = (name for name in store if name.startswith(key))
-            print('\n'.join(matching_entries))
-        else:
-            print('\n'.join(store))
-###^ Commands ^###
+        set_parser = subparsers.add_parser('set', help='set help')
+        set_parser.add_argument('key')
+        set_parser.add_argument('value', nargs='?')
+        set_parser.set_defaults(func=blast.cmd_set)
 
-def main():
-    parser = argparse.ArgumentParser(prog='blast', 
-                                    description='Your command line key-value store')
-    subparsers = parser.add_subparsers(help='Sub-commands')
+        delete_parser = subparsers.add_parser('delete', help='delete help')
+        delete_parser.add_argument('key')
+        delete_parser.set_defaults(func=blast.cmd_delete)
 
-    get_parser = subparsers.add_parser('get', help='get help', description='Get value')
-    get_parser.add_argument('key')
-    get_parser.set_defaults(func=cmd_get)
+        list_parser = subparsers.add_parser('list', help='list help')
+        list_parser.add_argument('key', nargs='?')
+        list_parser.set_defaults(func=blast.cmd_list)
 
-    set_parser = subparsers.add_parser('set', help='set help')
-    set_parser.add_argument('key')
-    set_parser.add_argument('value', nargs='?')
-    set_parser.set_defaults(func=cmd_set)
-
-    delete_parser = subparsers.add_parser('delete', help='delete help')
-    delete_parser.add_argument('key')
-    delete_parser.set_defaults(func=cmd_delete)
-
-    list_parser = subparsers.add_parser('list', help='list help')
-    list_parser.add_argument('key', nargs='?')
-    list_parser.set_defaults(func=cmd_list)
-
-    args = parser.parse_args()
-    args.func(args)
+        args = parser.parse_args(args)
+        args.func(args)
 
 if __name__ == '__main__':
     main()
--- tests.py
+++ tests.py
@@ -1,56 +1,252 @@
 import unittest
 import os
+import sys
+import io
 
 import blast
+
 TMP_FILE = '__test_tempfile'
-blast.Store.DB_PATH = TMP_FILE
+def cleanup():
+    try:
+        os.remove(TMP_FILE)
+    except OSError:
+        pass
 
-class TestStore(unittest.TestCase):
+class TestBlast(unittest.TestCase):
     def setUp(self):
-        self.store = blast.Store()
-
-    def test_dict(self):
-        store = self.store
-
-        n = 42
-        store['a'] = n
-        self.assertEqual(store['a'], n)
-
-        s = 'A string with russian словами'
-        store['b'] = s
-        self.assertEqual(store['b'], s)
-
-        v = [1, 2, None, 'Bacon', {42, 666}, ('Tuples!', ('Of Tuples!', 'of size 2'))]
-        store['c'] = v
-        self.assertEqual(store['c'], v)
-
-    def test_iter(self):
-        store = self.store
-
-        store['a'] = 1
-        store['b'] = 0
-        self.assertEqual(len(store), 2)
-
-        del store['a']
-        del store['b']
-        self.assertEqual(len(store), 0)
-
-        store['c'] = 42
-        self.assertTrue('c' in store)
-
-        del store['c']
-        self.assertTrue('c' not in store)
-
-        store['k1'] = 0
-        store['k2'] = 0
-        store['k3'] = 0
-        self.assertEqual(set(store), {'k1', 'k2', 'k3'})
-
-        del store['k1']
-        del store['k2']
-        del store['k3']
-        self.assertEqual(set(store), set())
+        self.blast = blast.Blast(TMP_FILE)
 
     def tearDown(self):
-        self.store.close()
-        os.remove(TMP_FILE)
+        self.blast.close()
+        cleanup()
+
+    def test_dict(self):
+        blast = self.blast
+
+        n = 42
+        blast['a'] = n
+        self.assertEqual(blast['a'], n)
+
+        s = 'A string with russian словами'
+        blast['b'] = s
+        self.assertEqual(blast['b'], s)
+
+        v = [1, 2, None, 'Bacon', {42, 666}, ('Tuples!', ('Of Tuples!', 'of size 2'))]
+        blast['c'] = v
+        self.assertEqual(blast['c'], v)
+
+    def test_iter(self):
+        blast = self.blast
+
+        blast['a'] = 1
+        blast['b'] = 0
+        self.assertEqual(len(blast), 2)
+
+        blast.clear()
+        self.assertEqual(len(blast), 0)
+
+        blast['c'] = 42
+        self.assertTrue('c' in blast)
+
+        del blast['c']
+        self.assertTrue('c' not in blast)
+
+        blast['k1'] = 0
+        blast['k2'] = 0
+        blast['k3'] = 0
+        self.assertEqual(set(blast), {'k1', 'k2', 'k3'})
+
+        blast.clear()
+        self.assertEqual(set(blast), set())
+
+    def test_list(self):
+        blast = self.blast
+
+        blast['a'] = 0
+        blast['b'] = 0
+        blast['n.a'] = 0
+        blast['n.b'] = 0
+        self.assertEqual(blast.get_list(), ['a', 'b', 'n.a', 'n.b'])
+        self.assertEqual(blast.get_list('a'), [])
+        self.assertEqual(blast.get_list('n'), ['n.a', 'n.b'])
+        self.assertEqual(blast.get_list('n.b'), [])
+
+        blast.clear()
+        self.assertEqual(blast.get_list(), [])
+        self.assertEqual(blast.get_list('a'), [])
+        self.assertEqual(blast.get_list('a.b'), [])
+
+    def test_validation(self):
+        blast = self.blast
+
+        self.assertTrue(blast.is_valid_key('a'))
+        self.assertTrue(blast.is_valid_key('wordy'))
+        self.assertTrue(blast.is_valid_key('борщ'))
+        self.assertTrue(blast.is_valid_key('word1.word2'))
+        self.assertTrue(blast.is_valid_key('борщ.матрёшка'))
+
+        self.assertFalse(blast.is_valid_key('word1 word2'))
+        self.assertFalse(blast.is_valid_key('wo.rd1 word2'))
+        self.assertFalse(blast.is_valid_key('word.'))
+        self.assertFalse(blast.is_valid_key('word1.word2.word3'))
+        self.assertFalse(blast.is_valid_key('word1.word2.'))
+        self.assertFalse(blast.is_valid_key('.word'))
+        self.assertFalse(blast.is_valid_key('word1.wor d2'))
+        self.assertFalse(blast.is_valid_key('...word1'))
+        self.assertFalse(blast.is_valid_key('word1..word2'))
+
+
+class TestOutput(unittest.TestCase):
+    @staticmethod
+    def mk_fake(**dct):
+        class FakeArgs:
+            pass
+        f = FakeArgs()
+        f.__dict__ = dct
+        return f
+    
+    def set_fake_buffer(self):
+        self.fake_stdout = io.StringIO()
+        sys.stdout = self.fake_stdout
+
+    def setUp(self):
+        self.blast = blast.Blast(TMP_FILE)
+        self.old_stdout = sys.stdout
+        self.set_fake_buffer()
+
+    def tearDown(self):
+        sys.stdout = self.old_stdout
+        self.fake_stdout.close()
+        self.blast.close()
+        cleanup()
+
+    def test_get(self):
+        blast = self.blast
+
+        blast.cmd_set(self.mk_fake(key='a', value='42'))
+        blast.cmd_get(self.mk_fake(key='a'))
+        self.assertEqual(self.fake_stdout.getvalue(), '42\n')
+        self.set_fake_buffer()
+
+        blast.cmd_set(self.mk_fake(key='b.c', value='42'))
+        blast.cmd_get(self.mk_fake(key='b.c'))
+        self.assertEqual(self.fake_stdout.getvalue(), '42\n')
+        self.set_fake_buffer()
+
+        blast.cmd_get(self.mk_fake(key='nonexistent'))
+        self.assertIn('Error', self.fake_stdout.getvalue())
+
+    def test_delete(self):
+        blast = self.blast
+
+        blast.cmd_set(self.mk_fake(key='a', value='42'))
+        blast.cmd_get(self.mk_fake(key='a'))
+        self.assertEqual(self.fake_stdout.getvalue(), '42\n')
+        self.set_fake_buffer()
+
+        blast.cmd_delete(self.mk_fake(key='a'))
+        blast.cmd_get(self.mk_fake(key='a'))
+        self.assertIn('Error', self.fake_stdout.getvalue())
+        self.set_fake_buffer()
+
+        blast.cmd_set(self.mk_fake(key='b.c', value='42'))
+        blast.cmd_set(self.mk_fake(key='a', value='42'))
+        blast.cmd_get(self.mk_fake(key='b.c'))
+        self.assertEqual(self.fake_stdout.getvalue(), '42\n')
+        self.set_fake_buffer()
+
+        blast.cmd_get(self.mk_fake(key='a'))
+        self.assertEqual(self.fake_stdout.getvalue(), '42\n')
+        self.set_fake_buffer()
+
+        blast.cmd_delete(self.mk_fake(key='b'))
+        self.assertIn('Error', self.fake_stdout.getvalue())
+        self.set_fake_buffer()
+
+        blast.cmd_delete(self.mk_fake(key='a'))
+        blast.cmd_get(self.mk_fake(key='b.c'))
+        self.assertEqual(self.fake_stdout.getvalue(), '42\n')
+        self.set_fake_buffer()
+
+        blast.cmd_delete(self.mk_fake(key='b.c'))
+        blast.cmd_get(self.mk_fake(key='b.c'))
+        self.assertIn('Error', self.fake_stdout.getvalue())
+        self.set_fake_buffer()
+
+    def test_list(self):
+        blast = self.blast
+
+        blast.cmd_set(self.mk_fake(key='a', value='42'))
+        blast.cmd_set(self.mk_fake(key='b', value='42'))
+        blast.cmd_set(self.mk_fake(key='k.a', value='42'))
+        blast.cmd_set(self.mk_fake(key='k.b', value='42'))
+        blast.cmd_set(self.mk_fake(key='k2.a', value='42'))
+        blast.cmd_list(self.mk_fake(key=None))
+        self.assertEquals(self.fake_stdout.getvalue(), 'a\nb\nk.a\nk.b\nk2.a\n')
+        self.set_fake_buffer()
+
+        blast.cmd_list(self.mk_fake(key='k'))
+        self.assertEquals(self.fake_stdout.getvalue(), 'k.a\nk.b\n')
+        self.set_fake_buffer()
+
+        blast.cmd_list(self.mk_fake(key='k.a'))
+        self.assertIn('no entries', self.fake_stdout.getvalue())
+        self.set_fake_buffer()
+
+        blast.cmd_list(self.mk_fake(key='a'))
+        self.assertIn('no entries', self.fake_stdout.getvalue())
+        self.set_fake_buffer()
+
+        blast.cmd_delete(self.mk_fake(key='a'))
+        blast.cmd_delete(self.mk_fake(key='b'))
+        blast.cmd_delete(self.mk_fake(key='k.a'))
+        blast.cmd_delete(self.mk_fake(key='k.b'))
+        blast.cmd_delete(self.mk_fake(key='k2.a'))
+        blast.cmd_list(self.mk_fake(key=None))
+        self.assertIn('no entries', self.fake_stdout.getvalue())
+        self.set_fake_buffer()
+
+        blast.cmd_list(self.mk_fake(key='a'))
+        self.assertIn('no entries', self.fake_stdout.getvalue())
+        self.set_fake_buffer()
+
+    def test_validation(self):
+        blast = self.blast
+
+        with self.assertRaises(SystemExit):
+            blast.cmd_set(self.mk_fake(key='a.b.c', value='42'))
+
+
+class TestMain(unittest.TestCase):
+    def set_fake_buffer(self):
+        self.fake_stdout = io.StringIO()
+        sys.stdout = self.fake_stdout
+
+    def setUp(self):
+        self.old_stdout = sys.stdout
+        self.set_fake_buffer()
+        self.old_db_path = blast.Blast.DEFAULT_DB_PATH
+        blast.Blast.DEFAULT_DB_PATH = TMP_FILE
+
+    def tearDown(self):
+        sys.stdout = self.old_stdout
+        self.fake_stdout.close()
+        blast.Blast.DEFAULT_DB_PATH = self.old_db_path
+        cleanup()
+
+    def test_get(self):
+        blast.main(['set', 'a', '42'])
+        blast.main(['get', 'a'])
+        self.assertEqual(self.fake_stdout.getvalue(), '42\n')
+
+    def test_delete(self):
+        blast.main(['set', 'a', '42'])
+        blast.main(['delete', 'a'])
+        blast.main(['get', 'a'])
+        self.assertIn('Error', self.fake_stdout.getvalue())
+
+    def test_list(self):
+        blast.main(['set', 'a', '42'])
+        blast.main(['set', 'k.b', '42'])
+        blast.main(['list'])
+        self.assertEqual(self.fake_stdout.getvalue(), 'a\nk.b\n')
